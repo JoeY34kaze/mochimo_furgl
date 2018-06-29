de2fc32efbaeb11ec86e65a060cf274548f386fe
Joey34kaze <ragnar1724@gmail.com>
2018-06-29 17:45:17 +0200

    start
    ztdr

---
+++ awsStart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##usage
+## curl -sSL https://raw.githubusercontent.com/JoeY34kaze/mochimo/master/awsStart.sh | bash 
+## A.B. 2018
+git clone https://github.com/JoeY34kaze/mochimo
+chmod -R a+rwx mochimo
+
+cat << EOF > ~/runminer.sh
+#/bin/bash
+cd mochimo/mochimo/mochi/bin/
+~/mochimo/mochimo/mochi/bin/gomochi d -t3 >> ~/mochi.log
+EOF
+chmod 777 ~/runminer.sh
+screen -dmS Miner -c `~/runminer.sh`

9bb226f0862c6c2545da244fb625d9e11c56d4d3
Joey34kaze <ragnar1724@gmail.com>
2018-06-29 17:47:45 +0200

    Update awsStart.sh

--- awsStart.sh
+++ awsStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##usage
-## curl -sSL https://raw.githubusercontent.com/JoeY34kaze/mochimo/master/awsStart.sh | bash 
+## curl -sSL https://raw.githubusercontent.com/JoeY34kaze/mochimo_furgl/master/awsStart.sh | bash 
 ## A.B. 2018
 git clone https://github.com/JoeY34kaze/mochimo_furgl
 chmod -R a+rwx mochimo
